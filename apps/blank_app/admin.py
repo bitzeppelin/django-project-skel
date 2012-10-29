d1776129d42deb2f0a36f1c34bbec98901d4122d
Hansy Schmitt Camacho <hansy@bitzeppelin.com>
2012-10-29 14:43:32 -0500

    Adding app skeleton (blank_app)

---
+++ apps/blank_app/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+
+#from someapp.models import Something
+
+#class SomethingAdmin(admin.ModelAdmin):
+#    list_display = ('id', 'field2', 'field3', 'field4', ) 
+
+#admin.site.register(Something, SomethingAdmin)
---
+++ apps/blank_app/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls.defaults import patterns, include, url
+
+#from someapp import views
+
+#urlpatterns = patterns('',
+
+      #url(r'^$',
+      #  views.some_view,
+      #  name='some_view'),
+
+#)
