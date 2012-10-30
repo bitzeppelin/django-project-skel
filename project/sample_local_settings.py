e0d9d007cb9d5d6249cd65edcba67c983cd6fc55
Antonio Ognio <antonio@bitzeppelin.com>
2012-10-29 20:35:24 -0500

    Refactoring the sample_local_settings.py to follow the hack at http://www.robgolding.com/blog/2010/05/03/extending-settings-variables-with-local_settings-py-in-django/

--- project/sample_local_settings.py
+++ project/sample_local_settings.py
@@ -1,15 +1,3 @@
-DEBUG=True
+LOCAL_SETTINGS = True
+from settings import *
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-if CLOSED_BETA_ACTIVE = False:
-    MIDDLEWARE_CLASSES = (
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.middleware.locale.LocaleMiddleware',
-            'django.middleware.common.CommonMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.messages.middleware.MessageMiddleware',
-    )
